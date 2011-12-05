1e2b5d318b64f4f019d4981a225832ff63a7e0ed
Ryan Romanchuk <rromanchuk@gmail.com>
2011-12-04 17:00:45 -0800

    stub a little more of the interface out

--- assets/application.js
+++ assets/application.js
@@ -4,9 +4,18 @@
 
   Calendar = (function() {
 
+    Calendar.monthOffsets;
+
+    Calendar.yearOffsets;
+
     function Calendar(month, year) {
-      this.didSelectMonth = __bind(this.didSelectMonth, this);
-      this.didSelectYear = __bind(this.didSelectYear, this);      this.dayLabels = ['Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat', 'Sun'];
+      this.monthScrollDidStart = __bind(this.monthScrollDidStart, this);
+      this.monthScrollDidDrag = __bind(this.monthScrollDidDrag, this);
+      this.monthScrollDidStop = __bind(this.monthScrollDidStop, this);
+      this.yearScrollDidStart = __bind(this.yearScrollDidStart, this);
+      this.yearScrollDidDrag = __bind(this.yearScrollDidDrag, this);
+      this.yearScrollDidStop = __bind(this.yearScrollDidStop, this);
+      this.scrollTo = __bind(this.scrollTo, this);      this.dayLabels = ['Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat', 'Sun'];
       this.monthLabels = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
       this.daysInMonth = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
     }
@@ -14,20 +23,71 @@
     Calendar.prototype.setupScrollBars = function() {
       $("#draggable-month").draggable({
         snap: ".scroll-bar#month",
-        axis: 'y'
+        axis: 'y',
+        start: this.monthScrollDidStart,
+        stop: this.monthScrollDidStop,
+        drag: this.monthScrollDidDrag,
+        opacity: 0.70
       });
       return $("#draggable-year").draggable({
         snap: ".scroll-bar#year",
-        axis: 'y'
+        axis: 'y',
+        start: this.yearScrollDidStart,
+        stop: this.monthScrollDidStop,
+        drag: this.yearScrollDidDrag,
+        opacity: 0.70
       });
     };
 
-    Calendar.prototype.didSelectYear = function(event) {
-      return console.log('didSelectYear');
+    Calendar.prototype.getMonthOffsets = function() {
+      var month, _i, _len, _ref, _results;
+      _ref = this.monthLabels;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        month = _ref[_i];
+        _results.push(this.monthOffsets.push($("[month=" + month + "]").offset()));
+      }
+      return _results;
     };
 
-    Calendar.prototype.didSelectMonth = function(event) {
-      return console.log('didSelectMonth');
+    Calendar.prototype.scrollTo = function(date) {
+      return console.log('scroll to' + date);
+    };
+
+    Calendar.prototype.yearScrollDidStop = function(event, ui) {
+      return console.log('yearScrollDidStop');
+    };
+
+    Calendar.prototype.yearScrollDidDrag = function(event, ui) {
+      return console.log('yearScrollDidStop');
+    };
+
+    Calendar.prototype.yearScrollDidStart = function(event, ui) {
+      return console.log('yearScrollDidStop');
+    };
+
+    Calendar.prototype.monthScrollDidStop = function(event, ui) {
+      return console.log('monthScrollDidStop');
+    };
+
+    Calendar.prototype.monthScrollDidDrag = function(event, ui) {
+      return console.log('monthScrollIsScrolling');
+    };
+
+    Calendar.prototype.monthScrollDidStart = function(event, ui) {
+      return console.log('monthScrollDidStart');
+    };
+
+    Calendar.prototype.isOverlapping = function(elem, offset) {
+      var h, l, maxx, maxy, t, w;
+      offset = $(elem).offset();
+      l = offset.left;
+      t = offset.top;
+      h = elem.height();
+      w = elem.width();
+      maxx = l + w;
+      maxy = t + h;
+      return (offset.y <= maxy && offset.y >= t) && (offset.x <= maxx && offset.x >= l);
     };
 
     Calendar.prototype.checkForLeapYear = function() {
--- lib/javascripts/application.coffee
+++ lib/javascripts/application.coffee
@@ -1,4 +1,7 @@
 class Calendar
+	@monthOffsets
+	@yearOffsets
+	
 	constructor:(month, year)->
 		@dayLabels = ['Mon', 'Tue', 'Wed', 'Thr', 'Fri', 'Sat', 'Sun']
 		@monthLabels = ['January', 'February', 'March', 'April',
@@ -10,15 +13,55 @@ class Calendar
 		$( "#draggable-month" ).draggable 
 			snap: ".scroll-bar#month"
 			axis: 'y'
+			start: @monthScrollDidStart
+			stop: @monthScrollDidStop
+			drag: @monthScrollDidDrag
+			opacity: 0.70
+		
 		$( "#draggable-year" ).draggable
 			snap: ".scroll-bar#year"
 			axis: 'y'
+			start: @yearScrollDidStart
+			stop: @monthScrollDidStop
+			drag: @yearScrollDidDrag
+			opacity: 0.70
 		
-	didSelectYear:(event)=>
-		console.log 'didSelectYear'
+	
+	getMonthOffsets:->
+		for month in @monthLabels
+			@monthOffsets.push $("[month=#{month}]").offset()
+			
+	scrollTo:(date)=>
+		console.log 'scroll to' + date
+	
+	yearScrollDidStop:(event, ui)=>
+		console.log 'yearScrollDidStop'
 		
-	didSelectMonth:(event)=>
-		console.log 'didSelectMonth'
+		
+	yearScrollDidDrag:(event, ui)=>
+		console.log 'yearScrollDidStop'
+
+	yearScrollDidStart:(event, ui)=>
+		console.log 'yearScrollDidStop'
+	
+	monthScrollDidStop:(event, ui)=>
+		console.log 'monthScrollDidStop'
+	
+	monthScrollDidDrag:(event, ui)=>
+		console.log 'monthScrollIsScrolling'
+		
+	monthScrollDidStart:(event, ui)=>
+		console.log 'monthScrollDidStart'
+		
+	isOverlapping: (elem, offset)->
+		offset = $(elem).offset();
+		l = offset.left
+		t = offset.top
+		h = elem.height()
+		w = elem.width()
+		maxx = l + w
+		maxy = t + h
+		return (offset.y <= maxy && offset.y >= t) && (offset.x <= maxx && offset.x >= l)
 		
 	checkForLeapYear:->
 		if @month == 1
